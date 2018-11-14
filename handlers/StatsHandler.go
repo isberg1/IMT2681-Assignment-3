d00615b60b4d9e78d95ee80623fc02308a76d8b7
isberg <isberg@msi.com>
2018-11-14 22:09:06 +0100

    statistic functonaliay redy to be tested in heroku

--- handlers/StatsHandler.go
+++ handlers/StatsHandler.go
@@ -45,33 +45,9 @@ func Statistic(str string)  {
 
 	_, ok := teller[str]
 	if !ok {
-		teller[str] = 0
+		teller[str] = 1
 	} else {
 		teller[str] += 1
 	}
-	// count up the usage for a g
 
-
-	/*
-	switch str {
-	case "joke":
-		teller[str] += 1
-	case "dad":
-		teller[str] += 1
-	case "cat gif":
-		teller[str] += 1
-	case "dog gif":
-		teller[str] += 1
-	case "hacker gif":
-		teller[str] += 1
-	case "trending gif":
-		teller[str] += 1
-	default:
-		logging("function called with incorrect string")
-	}
-
-	if len(teller) < 0 {
-		fmt.Print(teller[str])
-	}
-*/
 }
