2908ad309eceb95fe60464836f520b5cf07c174d
mats <moskjaer@stud.ntnu.no>
2018-11-17 17:15:56 +0100

    Try to change path again

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func main() {
 	r := mux.NewRouter()
 
 	// Allow static files (css and pictures) for the website.
-	r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./website"))))
 	// Serves the testing website.
-	r.Handle("/website.html", http.FileServer(http.Dir("./templates")))
+	r.Handle("/website.html", http.FileServer(http.Dir("./website/templates")))
 	// Handles the display of content in the website.
 	r.HandleFunc("/website.html/", website.APIContent).Methods("GET")
 
