b79f03004c62094a62e8073e41d3fa08e495ba44
Martin <martbraa@stud.ntnu.no>
2018-11-18 14:17:37 +0100

    oldposts website

--- handlers/dialogflowHandler.go
+++ handlers/dialogflowHandler.go
@@ -57,6 +57,7 @@ func Dialogflow(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	OldPosts(w, r, str.QueryResult.Parameters.B)
 	// select action based on request parameter
 	switch str.QueryResult.Parameters.B {
 	case "joke":
@@ -109,7 +110,7 @@ func Dialogflow(w http.ResponseWriter, r *http.Request) {
 	Statistic(str.QueryResult.Parameters.B)
 
 	// log request
-	postMemory = append(postMemory, string(read))
+	//	postMemory = append(postMemory, string(read))
 }
 
 /*
--- handlers/oldPostsHandler.go
+++ handlers/oldPostsHandler.go
@@ -3,17 +3,26 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // prints everything that has previously been posted  to "/dialogflow (used for input analyse and debugging)
-var postMemory []string
+// var postMemory []string
 
 // OldPosts prints all the preveous posts from dialogflow,
 // used for analyzing structure of json input, and debugging
-func OldPosts(w http.ResponseWriter, r *http.Request) {
+func OldPosts(w http.ResponseWriter, r *http.Request, cmd string) {
 
-	for _, val := range postMemory {
-		fmt.Fprintln(w, val)
-	}
+	/*
+		for _, val := range postMemory {
+			fmt.Fprintln(w, val)
+		}
+	*/
+	var commands string
+	commands = strings.Join([]string{
+		commands, "\n",
+		cmd, "\n"},
+		"")
 
+	fmt.Fprintln(w, commands)
 }
