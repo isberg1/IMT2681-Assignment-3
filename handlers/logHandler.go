8f51930c2cd3a1940204171d6f4eab4188bf0d54
Martin <martbraa@stud.ntnu.no>
2018-11-18 16:24:52 +0100

    changes to error logging

--- handlers/logHandler.go
+++ handlers/logHandler.go
@@ -3,23 +3,27 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 )
 
 // the logging entries can possibly be moved to a database
 
 // stores all log messages
-var logArray []string
+var logArray string
+var logTimestamp string
 
 // Log displays all log messages, used for storing errors, and debugging
 func Log(w http.ResponseWriter, r *http.Request) {
 
-	for _, val := range logArray {
-		fmt.Fprintln(w, val)
-	}
+	fmt.Fprintln(w, logArray)
 
 }
 
 // adds new log messages to storage
 func logging(s string) {
-	logArray = append(logArray, s)
+	logTimestamp = time.Now().String()
+	logArray = strings.Join([]string{
+		logTimestamp, "\t", logArray, "\n"},
+		"")
 }
